1b9b17411ede39fddb682b2e48612125ffe96b2b
EdwardsAlan <alan.edwards1103@gmail.com>
2019-02-24 22:08:02 -0500

    Update edwards-assignment-1.3.js

--- week 1/edwards-assignment-1.3.js
+++ week 1/edwards-assignment-1.3.js
@@ -1,6 +1,6 @@
 var url = require('url');
 
-var parsedURL = url.parse('http://www.example.com/profile?name=smith');
+var parsedURL = url.parse('http://www.edwardsexample.com/profile?name=edwards');
 
 console.log(parsedURL.protocol);
 console.log(parsedURL.host);
