3e7f7df07320fad872fdf866560b2dc2eb3d417d
EdwardsAlan <alan.edwards1103@gmail.com>
2019-03-17 21:29:44 -0400

    Create edwards.assignment.4.3.js

---
+++ week 4/status codes/edwards.assignment.4.3.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var http =  require('http');
+var logger = require('morgan');
+
+var app = express ();
+
+app.use(logger('dev'));
+
+
+//requests using status codes
+
+app.get('/not-found', function(require, response) {
+
+    response.status(404);
+
+    response.json({
+        error: 'Resource not found.'
+    });
+});
+
+app.get('/ok', function(require, response){
+
+    response.json({
+        error: "Page loaded correctly."
+    });
+});
+
+app.get('/not-implemented', function(require, response){
+
+    response.status(500);
+    response.json({
+        error: "Page not implemented."
+    });
+});
+
+http.createServer(app).listen(3000, function(){
+    console.log('Application is now listening on port 3000');
+});
