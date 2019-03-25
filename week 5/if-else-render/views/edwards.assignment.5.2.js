81867fd286bb89a0a9a98e50c0fffc2e9b619386
EdwardsAlan <alan.edwards1103@gmail.com>
2019-03-24 22:59:31 -0400

    Create edwards.assignment.5.2.js

---
+++ week 5/if-else-render/views/edwards.assignment.5.2.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+var composers = [
+
+  "Bach",
+  "Mozart",
+  "Beethoven",
+  "Verdi"
+
+];
+
+//routes
+app.get('/', function(request, response) {
+
+    response.render('index', {
+        names: composers
+
+    });
+
+});
+
+http.createServer(app).listen(3000, function() {
+
+    console.log("Application started on port 3000!");
+
+});
+
