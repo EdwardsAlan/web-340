7dc2d8e91f4e5340d93226d9ea2897fcd74322aa
EdwardsAlan <alan.edwards1103@gmail.com>
2019-03-24 22:55:32 -0400

    Create edwards.assignment.5.3.js

---
+++ week 5/Pug-Template/Views/edwards.assignment.5.3.js
@@ -0,0 +1,23 @@
+//require
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var pug = require('pug');
+
+var app = express();
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine','pug');
+
+//route
+app.get('/', function(require, response) {
+    resizeBy.render('index', {
+        message: 'Strive not to be a success, but rather to be of value - Albert Einstein'
+    });
+});
+
+//
+http.createServer(app).listen(3003, function() {
+
+    console.log("Application started on port 3003!");
+
+});
