266fb863e721e4b85ee0c39bb2649416b8604685
EdwardsAlan <alan.edwards1103@gmail.com>
2019-04-07 20:52:44 -0400

    Create edwards.assignment.7.3.js

---
+++ week 7/chai-example/test/edwards.assignment.7.3.js
@@ -0,0 +1,17 @@
+/*
+============================================
+; Title:  fruit.js
+; Author: Professor Krasso
+; Date:   28 February 2019
+; Modified by: Alan Edwards
+; Modified on 7 April 2019
+; Description: Function used in a Chai test.
+;===========================================
+*/
+function fruits(str) {
+
+    return str.split(',');
+
+}
+
+module.exports = fruits;
