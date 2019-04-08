f647ef197ca3f6409b4c777f9603e6d9d390f007
EdwardsAlan <alan.edwards1103@gmail.com>
2019-04-08 08:47:31 -0400

    Update edwards.assignment.4.2.js

--- week 4/json.api/edwards.assignment.4.2.js
+++ week 4/json.api/edwards.assignment.4.2.js
@@ -1,3 +1,14 @@
+/*
+============================================
+; Title:  app.js
+; Author: Professor Krasso
+; Date:   28 February 2019
+; Modified by:Alan Edwards
+; Description: Demonstrates how to return JSON from
+;              a Node.js server.
+;===========================================
+*/
+
 var express = require('express');
 var http = require ('http');
 var logger = require ('morgan');
